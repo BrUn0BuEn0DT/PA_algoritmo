f6ef688822bd30335679ddb04b465fd136410765
BrUn0BuEn0DT <141173420+BrUn0BuEn0DT@users.noreply.github.com>
2023-09-05 15:22:53 -0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+numero = 10
+
+nome = "bruno"
+
+booleano = True 
+
+a = 10
+b = 5
+soma = a+b
+subtracao = a-b
+multiplicacao = a*b
+divisao = a / b
+
+va = True
+vb = False
+
+r1 = va and vb
+r2 = va or vb
+r3 = not va 
+
+
+c = 3
+
+r4 = c > 4
+r5 = c == 4
+r6 = c != 5
+
+r7 = c > 2 and nome == "bruno" 
+
+r8 = b + c == 8
+
+r9 = a - b * c == -5
+
+r10 = a - b * c == 15
+
+r11 = a == 7 or b == 5
+
